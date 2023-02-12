dd225061ca8b07341796f3cc5c916d52c57e1b9a
Carlos Jefferson Braga Alves <carlos_jefferson_braga@outlook.com>
2023-02-12 00:58:10 -0400

    feat: creating JWT token controller

--- utils/exceptions/http_exceptions.py
+++ utils/exceptions/http_exceptions.py
@@ -11,3 +11,13 @@ class NotFound(BaseHTTPException):
         super().__init__(404, message, date)
 
 
+class Unauthorized(BaseHTTPException):
+    def __init__(self, message: str, date=None):
+        super().__init__(401, message, date)
+
+class Forbidden(BaseHTTPException):
+    def __init__(self, message: str, date=None):
+        super().__init__(403, message, date)
+
+
+
